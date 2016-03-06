ef43f4442fcfc4fb033457b4db1c91402493dda9
iminurnamez <easinerf@gmail.com>
2016-03-05 19:15:49 -0500

    wrong values in PLAYER_DEFAULT

--- data/components/player.py
+++ data/components/player.py
@@ -1,6 +1,6 @@
 from ..components.gang import Gang
 
-PLAYER_DEFAULT = ("Pythons", (127, 127, 127), ("Westdale"), {"Worker": 1, "Scavenger": 1, "Soldier": 2, "Raider": 5}, None)
+PLAYER_DEFAULT = ("Pythons", (127, 127, 127), ("Westdale"), {"Worker": 1, "Scavenger": 1, "Soldier": 1, "Raider": 1}, None)
 
 class Player(object):
     def __init__(self, city, info):
